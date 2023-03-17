37b8e0242b766c3787c18b45e66b62d0a8d02fad
DanushanKanagasingam <danushan@swivelgroup.com.au>
2023-03-17 20:02:30 +0530

    Changes Added

--- src/main/java/com/cms/gateway/CmsApiGatewayApplication.java
+++ src/main/java/com/cms/gateway/CmsApiGatewayApplication.java
@@ -3,8 +3,15 @@ package com.cms.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 @EnableEurekaClient
 public class CmsApiGatewayApplication {
 
@@ -12,9 +19,9 @@ public class CmsApiGatewayApplication {
         SpringApplication.run(CmsApiGatewayApplication.class, args);
     }
 
-//	@Bean
-//	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-//		return builder.routes().build();
-//	}
+    @Bean
+    public Docket authApi() {
+        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build();
+    }
 
 }
