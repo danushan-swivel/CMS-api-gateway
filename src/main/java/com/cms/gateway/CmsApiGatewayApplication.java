9441db198da83b27a599250aec6e4e3897836e02
DanushanKanagasingam <danushan@swivelgroup.com.au>
2023-03-18 10:44:42 +0530

    Added new location server url and fixed cors issue

--- src/main/java/com/cms/gateway/CmsApiGatewayApplication.java
+++ src/main/java/com/cms/gateway/CmsApiGatewayApplication.java
@@ -21,7 +21,8 @@ public class CmsApiGatewayApplication {
 
     @Bean
     public Docket authApi() {
-        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build();
+        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any()).build();
     }
 
 }
